9bb1b0fc957c91b2cccd455d441ccb7a32f0379c
Andrew <10902965+AndrewR3K@users.noreply.github.com>
2023-10-06 16:37:24 -0700

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,10 @@ fx_version "adamant"
 games { "rdr3" }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+lua54 'yes'
+author 'Jannings'
+
+
 server_scripts {
 	'server.lua',
 	'@oxmysql/lib/MySQL.lua'
@@ -14,3 +18,5 @@ client_scripts {
 
 
 export 'CheckPosseArea'
+
+version '1.1.2'
